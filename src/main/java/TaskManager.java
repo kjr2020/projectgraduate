332cb50e692e4bc0d06c10433f67258f3986b278
kjr2020 <kjr202020@gmail.com>
2022-06-20 21:12:49 +0900

    Add Files

---
+++ src/main/java/TaskManager.java
@@ -0,0 +1,17 @@
+public class TaskManager {
+
+    static void main(String args[]){
+        if(args.length < 2)
+            System.out.println("No argument args[0] = yamlFile, args[1] = executeFile");
+        else {
+            String containerYamlFile = args[0];
+            String taskExecuteFile = args[1];
+        }
+        int cpuLimit = 0;
+        int memLimit = 0;
+    }
+
+    public void getExecuteTime(){
+        
+    }
+}
