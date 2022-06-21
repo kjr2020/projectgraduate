085d1f8663ef9e3a22c957f19d7a0ae437a09d10
kjr2020 <kjr202020@gmail.com>
2022-06-21 15:46:53 +0900

    Add Code (06/20)

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,17 +1,54 @@
-public class TaskManager {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-    static void main(String args[]){
-        if(args.length < 2)
-            System.out.println("No argument args[0] = yamlFile, args[1] = executeFile");
-        else {
-            String containerYamlFile = args[0];
-            String taskExecuteFile = args[1];
+public class TaskManager {
+    private static final Logger LOG = LoggerFactory.getLogger(TaskManager.class);
+    private String yamlFile;
+    private String executeFile;
+    private int cpuLimit;
+    private int memLimit;
+
+    public TaskManager(String args[]){
+        this.yamlFile = args[0];
+        this.executeFile = args[1];
+        this.cpuLimit = 0;
+        this.memLimit = 0;
+    }
+
+    public static void main(String args[]){
+        TaskManager taskManager;
+        boolean result = false;
+        try{
+            taskManager = new TaskManager(args);
+            if(!result){
+                LOG.info("Can't create TaskManager");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
-        int cpuLimit = 0;
-        int memLimit = 0;
+    }
+
+    public void craeteContainer(String containerName){
+        Process process;
+        try {
+            process = Runtime.getRuntime().exec("kubectl apply -f " + containerName);
+            process.waitFor();
+            process.destroy();
+        } catch (Exception e){
+            LOG.info("Fail create Container.");
+            e.printStackTrace();
+        }
+    }
+
+    public void changeScale(){
+
     }
 
     public void getExecuteTime(){
         
+    }
+
+    public void overProvisioning(){
+
     }
 }
